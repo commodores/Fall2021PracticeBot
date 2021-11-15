ba4da7164627ce3bae186c2be4d5d833d866b47b
commodore <darren.hagler@4cacademy.org>
2021-11-15 12:29:46 -0500

    yy

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -81,7 +81,7 @@ public final class Constants {
         public static final double kMaxModuleAngularSpeedRadiansPerSecond = 6 * (2 * Math.PI);
         public static final double kMaxModuleAngularAccelerationRadiansPerSecondSquared = 6 * (2 * Math.PI);
     
-        public static final double kPModuleTurningController = 1.24;//3.5
+        public static final double kPModuleTurningController = 3.5;//3.5
     
         public static final double kPModuleDriveController = 1.82;
 
